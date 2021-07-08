9f87371a7ed0bee636d0b884ce54039878f253e8
mdafsha9 <mdafsha9@gmail.com>
2021-07-08 17:28:52 +0530

    if disabled is false

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,8 @@
   <span>{{greet}} {{name}}</span><br>
   <span v-text="framework"></span>
   <div v-html="inner"></div>
-  <span v-bind:id="attributeId">Binding attributes</span>
+  <span v-bind:id="attributeId">Binding attributes</span><br>
+  <button v-bind:disabled="isDisabled">Win a prize!</button>
 </template>
 
 <script>
@@ -16,6 +17,9 @@ export default {
       framework:'Vue3 Project!',
       inner:'<b>Welcome to Vue3</b><br><i>Vue is a progressive framework. It is used to build dynamic UI and SPA</i>',
       attributeId: 'attribute',
+      isDisabled:false,
+
+
 
     }
   }
