bbbf8152c325813daba1d704231083ec1ae049fe
mdafsha9 <mdafsha9@gmail.com>
2021-07-08 14:40:44 +0530

    static binding with mustach interpolation

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <template>
-
+  <p>It's a Static Binding</p>
+  <p>Below name is dynamic binding</p>
+  <span>{{greet}} {{name}}</span>
 
 </template>
 
@@ -9,7 +11,9 @@ export default {
   name: 'App',
   data(){
     return{
-
+      greet:"Hello",
+      name:'Afsha',
+    
 
 
     }
